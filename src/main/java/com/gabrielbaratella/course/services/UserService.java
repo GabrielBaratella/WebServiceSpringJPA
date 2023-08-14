90a753c253724205a1809a590d8d7838c70bfcf2
Gabriel Baratella <gabrielbaratellaoliveira@gmail.com>
2023-08-13 21:25:57 -0300

    User delete

--- src/main/java/com/gabrielbaratella/course/services/UserService.java
+++ src/main/java/com/gabrielbaratella/course/services/UserService.java
@@ -23,4 +23,12 @@ public class UserService {
 		Optional<User> obj = userRepo.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return userRepo.save(obj);
+	}
+	
+	public void remove(Long id) {
+		userRepo.deleteById(id);
+	}
 }
